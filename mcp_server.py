81c3fdcc5f263aa58c0d8826f1376df44f1a38c1
GabrielGonsalez <gabrielhfg055@gmail.com>
2026-07-10 00:03:43 -0300

    refactor: melhoria nos docstrings das ferramentas/metodos definidos no arquivo mcp

--- mcp_server.py
+++ mcp_server.py
@@ -5,34 +5,32 @@ mcp = FastMCP("Biblioteca")
 
 @mcp.tool()
 def listar_livros():
-    """Lista todos os livros."""
-
+    """
+    Lista todos os livros cadastrados na biblioteca, incluindo ID, título e status de disponibilidade.
+    """
     resposta = requests.get("http://localhost:5000/livros")
     resposta.raise_for_status()
-
     return resposta.json()
 
 @mcp.tool()
 def buscar_livro(titulo: str):
-    """Busca um livro pelo título."""
-
+    """
+    Busca livros na biblioteca que contenham o termo ou título informado.
+    """
     resposta = requests.get(
         "http://localhost:5000/livros",
         params={"titulo": titulo}
     )
     resposta.raise_for_status()
-
     return resposta.json()
 
 @mcp.tool()
 def consultar_livro(id: int):
-    """Consulta um livro pelo ID."""
-
-    resposta = requests.get(
-        f"http://localhost:5000/livros/{id}"
-    )
+    """
+    Consulta os detalhes de um livro específico utilizando o seu ID numérico.
+    """
+    resposta = requests.get(f"http://localhost:5000/livros/{id}")
     resposta.raise_for_status()
-
     return resposta.json()
 
 if __name__ == "__main__":
